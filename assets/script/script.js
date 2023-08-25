bc6271e8eba93dc8d7acb2122000276a88400ff4
kyleochata <kyleochata@gmail.com>
2023-08-25 13:12:51 -0700

    added starter code

---
+++ assets/script/script.js
@@ -0,0 +1,11 @@
+/*
+homepage with a start button
+  -when you click the start button => game will begins
+  - click start -> timer starts counting down
+  - clear homescreen on start click
+
+show the user the game
+  - 
+*/
+
+const mainEl = document.querySelector('#main');
